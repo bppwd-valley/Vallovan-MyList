21c5aad00e9cbef0f912e2039bb3214b1b7423be
bppwd-donovan <37300281+bppwd-donovan@users.noreply.github.com>
2018-05-14 12:45:48 -0400

    a

--- MyList/src/mylist/MyList.java
+++ MyList/src/mylist/MyList.java
@@ -1,17 +1,10 @@
 
 package mylist;
 
-/**
- *
- * @author pwd11
- */
 public class MyList {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
