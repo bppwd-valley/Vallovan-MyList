7048b7427a57b61b00d6e25ec73ccc7968d9e925
bppwd-valley <36992134+bppwd-valley@users.noreply.github.com>
2018-05-16 12:54:53 -0400

    Generics
    Generics are super cool apparently

--- MyList/src/mylist/Base.java
+++ MyList/src/mylist/Base.java
@@ -3,24 +3,24 @@ package mylist;
 public class Base {
 
     public static void main(String[] args) {
-        MyList<Integer> list = new MyList();
-        list.addLast(1);
-        list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
-        list.addLast(5);
-        list.addLast(6);
-        list.addLast(7);
-        list.addLast(8);
+        MyList<String> list = new MyList();
+        list.addLast("Gus");
+        list.addLast("Valley");
+        list.addLast("+");
+        list.addLast("Colin");
+        list.addLast("Donovan");
+        list.addLast("=");
+        list.addLast("Guslin");
+        list.addLast("Vallovan");
         list.print();
         System.out.println("--------------------------------------");
-        list.insertAt(3, 300);
+        list.insertAt(3, "NO");
         list.print();
         System.out.println("--------------------------------------");
         list.remove(3);
         list.print();
         System.out.println("--------------------------------------");
-        list.addFirst(600);
+        list.addFirst("TEST");
         list.print();
         System.out.println("--------------------------------------");
         System.out.println(list.toString());
