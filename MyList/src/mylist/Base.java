8412500e48fa37a7df119a50b69c6975f05d6ce5
bppwd-valley <36992134+bppwd-valley@users.noreply.github.com>
2018-05-16 10:12:14 -0400

    a

--- MyList/src/mylist/Base.java
+++ MyList/src/mylist/Base.java
@@ -4,21 +4,32 @@ public class Base {
 
     public static void main(String[] args) {
         MyList list = new MyList();
-        list.add("Gus");
-        list.add("Valley");
-        list.add("+");
-        list.add("Colin");
-        list.add("Donovan");
-        list.add("=");
-        list.add("Guslin");
-        list.add("Vallovan");
+        list.addLast("Gus");
+        list.addLast("Valley");
+        list.addLast("+");
+        list.addLast("Colin");
+        list.addLast("Donovan");
+        list.addLast("=");
+        list.addLast("Guslin");
+        list.addLast("Vallovan");
         list.print();
         System.out.println("--------------------------------------");
-        list.insert(3, "NO");
+        list.insertAt(3, "NO");
         list.print();
         System.out.println("--------------------------------------");
         list.remove(3);
         list.print();
+        System.out.println("--------------------------------------");
+        list.addFirst("TEST");
+        list.print();
+        System.out.println("--------------------------------------");
+        System.out.println("Get first:");
+        System.out.println(list.getFirst());
+        System.out.println("--------------------------------------");
+        System.out.println("Get last:");
+        System.out.println(list.getLast());
+        System.out.println("--------------------------------------");
+        System.out.println(list.toString());
     }
     
 }
--- MyList/src/mylist/MyList.java
+++ MyList/src/mylist/MyList.java
@@ -8,7 +8,7 @@ public class MyList {
         head = new Node();   
     }
     
-    public void add(String value){
+    public void addLast(String value){
         if (head == null){
             head = new Node(value, null);
         }
@@ -29,9 +29,17 @@ public class MyList {
         }
     }
     
-    public void insert(int index, String newVal){
+    public void insertAt(int index, String newVal){
         Node tmp = head;
         for(int i = 0; i < index; i++){
+            tmp = tmp.next;
+        }
+        tmp.next = new Node(newVal, tmp.next);
+    }
+    
+    public void insertAfter(String target, String newVal){
+        Node tmp = head;
+        while(tmp.getValue() != target){
             tmp = tmp.next;
         }
         tmp.next = new Node(newVal, tmp.next);
@@ -45,5 +53,48 @@ public class MyList {
         tmp.next = tmp.next.next;
     }
     
+    public void addFirst(String value){
+        Node tmp = head;
+        tmp.next = new Node(value, tmp.next);
+    }
     
+    public String get(int index){
+        Node tmp = head;
+        for(int i = 0; i <index+1; i++){
+            tmp = tmp.next;
+        }
+        return tmp.getValue();
+    } 
+    
+    public String getFirst(){
+        Node tmp = head;
+        tmp = tmp.next;
+        if (head == null){
+            return "List is empty";
+        }
+        return tmp.getValue();
+    }
+    
+    public String getLast(){
+        Node tmp = head;
+        tmp = tmp.next;
+        if (head == null){
+            return "List is empty";
+        }
+        while(tmp.next != null){
+            tmp = tmp.next;
+        }
+        return tmp.getValue();
+    }
+    
+    @Override
+    public String toString(){
+        Node tmp = head;
+        String toS = "{ ";
+        while(tmp.next != null){
+            toS += tmp.getValue() + ", ";
+            tmp = tmp.next;
+        }
+        return toS + " }";
+    }
 }
